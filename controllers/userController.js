0aaf37ddc96421a252b10b44f8f324e46fb7038a
somitgarhewal <somitgarhewal@gmail.com>
2020-03-17 11:17:01 +0530

    fetch user details with id - api created

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,11 +6,17 @@ const user = mongoose.model('user');
 const userController = {
 
    getUser : async (req, res) => {
-      const data= await user.find({})
-      console.log("person ", data);
+      const userData= await user.find({})
+      console.log("person ", userData);
+      res.send({person: userData})
+   },
+   fetchUser : async (req, res) => {
+      const data= await user.find({ '_id': req.params.id })
+      console.log("id ", req.params.id);
+
+      console.log("persons ", data);
       res.send({person: data})
    }
-  
 }
 
 module.exports = userController;
--- routers/userRouters.js
+++ routers/userRouters.js
@@ -2,4 +2,5 @@ var User = require('../controllers/userController')
 
 module.exports = (router) => {
     router.get('/api/getUser', User.getUser);
+    router.get('/api/fetchUser/:id', User.fetchUser);
 }
